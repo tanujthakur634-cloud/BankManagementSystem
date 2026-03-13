c2c12fcb18f5a5cc1b95305b54adfe4f9081a5fa
Tanuj <tanujthakur634@gmail.com>
2026-03-13 08:14:51 +0530

    Updated Commit

--- src/bank/database/BankDatabase.java
+++ src/bank/database/BankDatabase.java
@@ -5,7 +5,7 @@ import bank.model.Account;
 import java.util.HashMap;
 
 public class BankDatabase {
-    private HashMap<Integer, Account> accounts = new HashMap<>();
+    private final HashMap<Integer, Account> accounts = new HashMap<>();
 
     public void addAccount(Account account) {
         accounts.put(account.getAccountNumber(), account);
